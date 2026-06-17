129b69abc0ce02cdd6b4fbfd113c9de58a9af71d
Taka005 <contact@takadev.jp>
2026-06-17 15:35:46 +0900

    Manifestの処理を調整

--- app/src/main/java/com/taka/encfilereader/model/Manifest.kt
+++ app/src/main/java/com/taka/encfilereader/model/Manifest.kt
@@ -6,42 +6,49 @@ import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.GCMParameterSpec
 import javax.crypto.spec.PBEKeySpec
 import javax.crypto.spec.SecretKeySpec
+import kotlin.comparisons.compareBy
 
-class Manifest (val path: String){
-    var rawData: ByteArray? = null
-    var salt: ByteArray? = null
-    var iv: ByteArray? = null
-    var tag: ByteArray? = null
+open class Manifest (val path: String){
     var key: SecretKeySpec? = null
-    val files: MutableList<String> = mutableListOf()
-    val fileCount: Int
+    var files: MutableList<FileMetaData> = mutableListOf()
+    open val fileCount: Int
         get() = files.size
 
-    fun setBuffer(data: ByteArray): Unit{
-        if (data.size < 44) throw IllegalArgumentException("データサイズが不足しています\n必要なサイズ: 44, 実際: ${data.size}")
+    fun setBuffer(data: ByteArray,password: String): Unit{
+        require(data.size >= 44) { "データサイズが不足しています。サイズ: ${data.size}" }
 
-        this.salt = data.sliceArray(0 until 16)
-        this.iv = data.sliceArray(16 until 28)
-        this.tag = data.sliceArray(28 until 44)
-        this.rawData = data.sliceArray(44 until data.size)
+        val salt: ByteArray = data.sliceArray(0 until 16)
+        val iv: ByteArray = data.sliceArray(16 until 28)
+        val tag: ByteArray = data.sliceArray(28 until 44)
+        val rawData: ByteArray = data.sliceArray(44 until data.size)
+
+        val decRawData = decryptData(rawData + tag, salt, iv, password)
+
+        this.files = Json.decodeFromString<MutableList<FileMetaData>>(decRawData)
+
+        sortFile()
     }
 
-    fun decrypt(password: String): Unit{
-        val currentSalt = salt ?: throw IllegalArgumentException("Saltが設定されていません")
-        val currentIv = iv ?: throw IllegalArgumentException("IVが設定されていません")
-        val currentTag = tag ?: throw IllegalArgumentException("Tagが設定されていません")
-        val currentRawData = rawData ?: throw IllegalArgumentException("rawDataが設定されていません")
-
-        val spec = PBEKeySpec(password.toCharArray(), currentSalt, 100000, 256)
+    fun decryptData(content: ByteArray, salt: ByteArray, iv: ByteArray, password: String): String {
+        val spec = PBEKeySpec(password.toCharArray(), salt, 100000, 256)
         val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256")
         this.key = SecretKeySpec(factory.generateSecret(spec).encoded, "AES")
 
         val cipher = Cipher.getInstance("AES/GCM/NoPadding")
-        val gcmSpec = GCMParameterSpec(128, currentIv)
-        cipher.init(Cipher.DECRYPT_MODE, this.key, gcmSpec)
+        cipher.init(Cipher.DECRYPT_MODE, this.key, GCMParameterSpec(128, iv))
 
-        val decRawData = String(cipher.doFinal(currentRawData + currentTag))
+        return String(cipher.doFinal(content))
+    }
 
-        val files = Json.decodeFromString<List<FileMetaData>>(decRawData)
+    fun sortFile(): Unit{
+        this.files.sortWith(
+            compareBy { it.originalFileName }
+        )
+
+        this.files.forEach { data ->
+            data.contents.sortWith(
+                compareBy({ it.name })
+            )
+        }
     }
 }
