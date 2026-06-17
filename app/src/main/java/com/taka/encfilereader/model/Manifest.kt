e5b46503b261aafed3dd4545e7152fdada731ad4
Taka005 <contact@takadev.jp>
2026-06-17 21:03:32 +0900

    Result型を導入

--- app/src/main/java/com/taka/encfilereader/model/Manifest.kt
+++ app/src/main/java/com/taka/encfilereader/model/Manifest.kt
@@ -14,33 +14,53 @@ open class Manifest (val path: String){
     open val fileCount: Int
         get() = files.size
 
-    fun setBuffer(data: ByteArray,password: String): Unit{
-        require(data.size >= 44) { "データサイズが不足しています。サイズ: ${data.size}" }
+    fun setBuffer(data: ByteArray,password: String): Result<Unit>{
+        if (data.size < 44) return Result.failure(IllegalArgumentException("データサイズ不足: ${data.size}"))
 
         val salt: ByteArray = data.sliceArray(0 until 16)
         val iv: ByteArray = data.sliceArray(16 until 28)
         val tag: ByteArray = data.sliceArray(28 until 44)
         val rawData: ByteArray = data.sliceArray(44 until data.size)
 
-        val decRawData = decryptData(rawData + tag, salt, iv, password)
+        this.key = createKey(salt,password).getOrElse { error ->
+            return Result.failure(error)
+        }
 
-        this.files = Json.decodeFromString<MutableList<FileMetaData>>(decRawData)
+        val decRawData = decryptData(rawData + tag, salt, iv, password).getOrElse { error ->
+            return Result.failure(error)
+        }
 
-        sortFile()
+        return runCatching {
+            this.files = Json.decodeFromString<MutableList<FileMetaData>>(decRawData)
+
+            sortFiles()
+        }
     }
 
-    fun decryptData(content: ByteArray, salt: ByteArray, iv: ByteArray, password: String): String {
-        val spec = PBEKeySpec(password.toCharArray(), salt, 100000, 256)
-        val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256")
-        this.key = SecretKeySpec(factory.generateSecret(spec).encoded, "AES")
+    fun getContent(data: ByteArray){
 
-        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
-        cipher.init(Cipher.DECRYPT_MODE, this.key, GCMParameterSpec(128, iv))
-
-        return String(cipher.doFinal(content))
     }
 
-    fun sortFile(): Unit{
+    fun decryptData(content: ByteArray, salt: ByteArray, iv: ByteArray, password: String): Result<String> {
+        return runCatching {
+            val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+            cipher.init(Cipher.DECRYPT_MODE, this.key, GCMParameterSpec(128, iv))
+
+            String(cipher.doFinal(content))
+        }
+    }
+
+    fun createKey(salt: ByteArray,password: String): Result<SecretKeySpec>{
+        return runCatching {
+            val spec = PBEKeySpec(password.toCharArray(), salt, 100000, 256)
+            val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256")
+
+            SecretKeySpec(factory.generateSecret(spec).encoded, "AES")
+        }
+
+    }
+
+    fun sortFiles(): Unit{
         this.files.sortWith(
             compareBy { it.originalFileName }
         )
