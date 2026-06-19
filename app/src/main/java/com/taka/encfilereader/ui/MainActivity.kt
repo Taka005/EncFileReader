646404cc59595fe5ba57be33059623ad0a6972a5
Taka005 <contact@takadev.jp>
2026-06-19 18:29:37 +0900

    coilを追加 テストの描画を追加

--- app/src/main/java/com/taka/encfilereader/ui/MainActivity.kt
+++ app/src/main/java/com/taka/encfilereader/ui/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,7 +19,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.taka.encfilereader.R
+import com.taka.encfilereader.ui.components.GridItem
 import com.taka.encfilereader.ui.theme.EncFileReaderTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,14 +38,14 @@ class MainActivity : ComponentActivity() {
                             title = { Text(stringResource(id = R.string.app_name)) },
                             colors = TopAppBarDefaults.topAppBarColors(
                                 containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                titleContentColor = MaterialTheme.colorScheme.primary
+                                titleContentColor = MaterialTheme.colorScheme.onPrimary
                             )
                         )
                     }
                 ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    ImageGridScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        columns = 2
                     )
                 }
             }
@@ -49,9 +55,16 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ImageGridScreen(modifier: Modifier = Modifier,columns: Int) {
+    val items = List(20){"test"}
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(columns),
+        contentPadding = PaddingValues(3.dp),
         modifier = modifier
-    )
+    ) {
+        items(items) {
+            GridItem()
+        }
+    }
 }
---
+++ app/src/main/java/com/taka/encfilereader/ui/components/GridItem.kt
@@ -0,0 +1,72 @@
+package com.taka.encfilereader.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.taka.encfilereader.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GridItem() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(3.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ){
+        Column{
+            AsyncImage(
+                model = R.drawable.placeholder_loading,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(4f/5f),
+                contentScale = ContentScale.Crop,
+                error = painterResource(R.drawable.ic_launcher_background),
+                placeholder = painterResource(R.drawable.ic_launcher_background)
+            )
+
+            Text(
+                text = "これはテスト",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp, top = 4.dp, bottom = 0.dp),
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+
+            )
+
+            Text(
+                text = "10ファイル",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp, top = 0.dp, bottom = 4.dp),
+                textAlign = TextAlign.Right,
+                style = MaterialTheme.typography.bodySmall,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
