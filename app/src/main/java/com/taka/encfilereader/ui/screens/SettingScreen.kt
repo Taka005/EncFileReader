59c408a99d0a02e8cb0d7a55be855c0613cb21fa
Taka005 <contact@takadev.jp>
2026-06-29 21:09:48 +0900

    表示を調整

--- app/src/main/java/com/taka/encfilereader/ui/screens/SettingScreen.kt
+++ app/src/main/java/com/taka/encfilereader/ui/screens/SettingScreen.kt
@@ -2,7 +2,6 @@ package com.taka.encfilereader.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -106,8 +105,7 @@ fun SettingScreen(
                 Button(
                     onClick = {
 
-                    },
-                    modifier = Modifier.fillMaxWidth()
+                    }
                 ) {
                     Text("リセット")
                 }
