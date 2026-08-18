057ca0128e4a2dd0498ac3e732793cf1042605ed
Taka005 <contact@takadev.jp>
2026-08-19 01:33:17 +0900

    リファクタリング

--- app/src/main/java/com/taka/encfilereader/ui/screens/FileListScreen.kt
+++ app/src/main/java/com/taka/encfilereader/ui/screens/FileListScreen.kt
@@ -1,19 +1,15 @@
 package com.taka.encfilereader.ui.screens
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -25,8 +21,6 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DrawerValue
@@ -56,22 +50,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavOptions
 import androidx.navigation.navOptions
-import coil3.compose.SubcomposeAsyncImage
 import com.taka.encfilereader.ui.components.FileItem
 import com.taka.encfilereader.ui.components.HistoryItem
 import com.taka.encfilereader.ui.components.OpenDialog
 import com.taka.encfilereader.ui.states.FileUiState
 import com.taka.encfilereader.ui.views.FileListViewModel
 import com.taka.encfilereader.ui.views.HistoryViewModel
-import com.taka.encfilereader.util.formatTimestamp
 import kotlinx.coroutines.launch
-import kotlin.math.round
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
